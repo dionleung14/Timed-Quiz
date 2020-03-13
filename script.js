bf97764a501fbdf49f4bf9110d7652d2d4159ba6
NA Flipper <dioncleung@gmail.com>
2020-03-12 23:48:50 -0700

    created initial files. verified link between script and html

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log("connected!");
+function testerFunction() {
+    console.log("pass");
+}
+
+// Grab start button
+var startBtnEl = document.getElementById("startQuiz");
+
+// Add click listener to start button
+startBtnEl.addEventListener("click",testerFunction);
+
+// Start a timer and countdown
+// Grab the timer element
+var timer = document.querySelector("#timer");
+
+// Grab countdown element
+var countdown = document.querySelector("#countdown");
+
+// Countdown ticker
+function ticker() {
+    var x = 5;
+    setTimeout
+}
